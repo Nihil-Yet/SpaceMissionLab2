2f46aa9184ccaf950d4c2074d7a3d813c69bb0bc
Nihil-Yet <rdbel_ozerov@outlook.com>
2026-04-07 18:56:02 +0300

    Вместо строк используются enum и int каст

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,18 +4,17 @@ namespace SpaceMission.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<MissionEntity> Missions { get; set; }
+        public DbSet<MissionEntity> Missions { get; set; } = null!;
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Таблица будет одна, но с разными полями
             modelBuilder.Entity<MissionEntity>()
-                .HasDiscriminator<string>("MissionType")
-                .HasValue<MissionEntity>("Orbital")
-                .HasValue<MissionEntity>("Planetary");
+                .HasDiscriminator<int>("MissionType")
+                .HasValue<MissionEntity>((int)Models.MissionT.Orbital)
+                .HasValue<MissionEntity>((int)Models.MissionT.Planetary);
         }
     }
 }
--- Data/MissionEntity.cs
+++ Data/MissionEntity.cs
@@ -2,26 +2,24 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SpaceMission.Data
 {
-    // Эта сущность хранится в БД
     public class MissionEntity
     {
         [Key]
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         public int Budget { get; set; }
         public int Duration { get; set; }
 
-        // Дискриминатор: "Orbital" или "Planetary"
-        public string MissionType { get; set; }
+        public int MissionType { get; set; }
 
         // Поля для OrbitalMission
         public double? CurrHeight { get; set; }
         public double? TargetHeight { get; set; }
         public double? Inclination { get; set; }
-        public string? EnergySource { get; set; }   // None, RTG, Solar
+        public int? EnergySource { get; set; }
 
         // Поля для PlanetaryMission
         public string? Planet { get; set; }
